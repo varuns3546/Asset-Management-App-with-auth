d0c68ef0b02f6345d2c6ae18a51b8ee588808d51
varuns3546 <varunseenivasan82786@gmail.com>
2025-07-29 09:22:24 -0500

    f

--- frontend/src/features/entries/entryService.js
+++ frontend/src/features/entries/entryService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {API_BASE_URL} from '@env'
 //const API_URL = API_BASE_URL+'/api/entries/'
-const API_URL = 'http://localhost:3000/api/entries/'
+const API_URL = 'https://asset-management-app-d48j.onrender.com/api/entries/'
 // Create new entry
 const createEntry = async (entryData, token) => {
   const config = {
